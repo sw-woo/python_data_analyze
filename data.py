796be1b4c20ba20bbe620d4b2c1250f2cee6460c
sw <swpheus1@gmail.com>
2020-09-25 18:40:02 +0900

    numpy

--- data.py
+++ data.py
@@ -3,10 +3,11 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # numpy 기초
-# x = np.array([1, 2, 3, 4, 5])
-# y = np.array([1, 2, 3, 4, 5])
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 3, 4, 5])
 # print(x)
 # print(y)
+
 
 plt.plot(x, y)
 plt.show()
