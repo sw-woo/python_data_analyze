ee619e0d2a094833b9785c09b01a3ef814558790
sw <swpheus1@gmail.com>
2020-09-25 15:46:55 +0900

    change second

---
+++ data.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 3, 4, 5])
+print(x)
+print(y)
+
+plt.plot
--- numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as mp
-
-
-#numpy 기초 학습
-
